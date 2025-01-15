a30669a9b9678038b6332da93f56ad47ed7dfffc
Niketdeshmukh <niketdeshmukh2002@gmail.com>
2025-01-16 01:03:11 +0530

    created server

--- techfest-backend/server.js
+++ techfest-backend/server.js
@@ -3,7 +3,7 @@ const connectDB = require("./config/db");
 const eventRoutes = require("./routes/eventRoutes");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const User = require("./models/User");
+const path = require("path");
 
 const app = express();
 const PORT = 5000;
@@ -14,19 +14,11 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/competitions", express.static(path.join(__dirname, "competitions")));
 
 // Routes
 app.use("/api", eventRoutes);
-app.post("/api/users", async (req, res) => {
-    try {
-      const { name, email, role } = req.body;
-      const user = new User({ name, email, role });
-      await user.save();
-      res.status(201).json(user);
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
-  });
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
