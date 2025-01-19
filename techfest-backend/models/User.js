ae8d86848b22e15b1cf62406667577d029b669e0
Niketdeshmukh <niketdeshmukh2002@gmail.com>
2025-01-19 22:54:04 +0530

    updated

--- techfest-backend/models/User.js
+++ techfest-backend/models/User.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   lastname: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   gender: { type: String, required: true },
   enrollment: { type: String, required: true },
-  phone: { type: String, required: true },
+  phone: { type: String, required: true, unique: true },
   age: { type: Number, required: true },
 });
 
