5a98eefc1a3a987307e1c79bf7373dd1312ecbc8
Niket deshmukh <niketdeshmukh2002@gmail.com>
2025-01-20 00:18:54 +0530

    Update server.js

--- techfest-backend/server.js
+++ techfest-backend/server.js
@@ -8,7 +8,9 @@ app.use(cors({
 
 app.use(express.json());
 app.use('/api/events', require('./routes/eventRoutes'));
-
+app.get('/', (req, res) => {
+  res.send('Welcome to the Techfest API');
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
