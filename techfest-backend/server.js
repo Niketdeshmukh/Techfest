0522547b0927c768bbcb5ec14d9df8dcaa7889bd
Niketdeshmukh <niketdeshmukh2002@gmail.com>
2025-01-23 01:53:30 +0530

    updated server.js

--- techfest-backend/server.js
+++ techfest-backend/server.js
@@ -2,18 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors({
-  origin: 'https://techfest-frontend-psi.vercel.app', // Your frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-  credentials: true, // Allow cookies if needed
-}));
+app.use(
+  cors({
+    origin: ['https://techfest-frontend-psi.vercel.app'], // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
+
 app.use('/api/events', require('./routes/eventRoutes'));
+
 app.get('/', (req, res) => {
   res.send('Welcome to the Techfest API');
 });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
