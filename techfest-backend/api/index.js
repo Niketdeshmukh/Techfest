515497a2a9a2fc22f6c3cebf6bf150f97715147e
Niketdeshmukh <niketdeshmukh2002@gmail.com>
2025-02-28 23:03:21 +0530

    added cors

--- techfest-backend/api/index.js
+++ techfest-backend/api/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://techfest-frontend-psi.vercel.app",
+    credentials: true,
+}));
 app.use(express.json());
 
 // Import routes
